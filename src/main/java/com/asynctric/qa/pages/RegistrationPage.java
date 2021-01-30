b66d575abea526341a0c969a9e22505fcecf3c09
ARUN DUBEY <arun5603@gmail.com>
2021-01-30 16:35:35 +0530

    Registration page added

--- src/main/java/com/asynctric/qa/pages/RegistrationPage.java
+++ src/main/java/com/asynctric/qa/pages/RegistrationPage.java
@@ -1,5 +1,133 @@
 package com.asynctric.qa.pages;
 
-public class RegistrationPage {
+import java.io.IOException;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.asynctric.qa.base.TestBase;
+
+public class RegistrationPage extends TestBase {
+
+	public RegistrationPage() throws IOException {
+		super();
+		PageFactory.initElements(driver, this);
+		// TODO Auto-generated constructor stub
+	}
+	
+// Page Factory --- OR
+@FindBy(linkText="Create Account")
+WebElement Registration;
+@FindBy(id="name")
+WebElement Name;
+@FindBy(id="email")
+WebElement Email;
+@FindBy(id="mobile")
+WebElement Mobile;
+@FindBy(id="password")
+WebElement Password;
+@FindBy(xpath="//span[@class='MuiButton-label']")
+WebElement Submit;
+@FindBy(xpath="//h1[contains(text(),'Signup with Asyncentric')]")
+WebElement RegistrationPageText;
+@FindBy(xpath="//span[@id='snackbar-message-id']")
+WebElement coformationmessage;
+@FindBy(linkText="Login!")
+WebElement Login;
+
+	public String ValidateRegPageURL()
+	{
+		Registration.click();
+		return driver.getCurrentUrl();
+	}
+	
+	public String ValidateRegPageText()
+	{
+		Registration.click();
+		return RegistrationPageText.getText();
+	}
+
+	public String RegFormName()
+	{
+		Registration.click();
+	//	Name.clear();
+	//	Name.sendKeys("Arun");
+		Email.clear();
+		Email.sendKeys("arun5603@gmail.com");
+		Mobile.clear();
+		Mobile.sendKeys("9899001068");
+		Password.clear();
+		Password.sendKeys("12345");
+		Submit.click();
+		
+		System.out.println(coformationmessage.getText());
+		return coformationmessage.getText();
+	}
+	public String RegFormEmail()
+	{
+		Registration.click();
+		Name.clear();
+		Name.sendKeys("Arun");
+	//	Email.clear();
+	//	Email.sendKeys("arun5603@gmail.com");
+		Mobile.clear();
+		Mobile.sendKeys("9899001068");
+		Password.clear();
+		Password.sendKeys("12345");
+		Submit.click();	
+		System.out.println(coformationmessage.getText());
+		return coformationmessage.getText();
+	}
+	public String RegFormMobile()
+	{
+		Registration.click();
+		Name.clear();
+		Name.sendKeys("Arun");
+		Email.clear();
+		Email.sendKeys("arun5603@gmail.com");
+	//	Mobile.clear();
+	//	Mobile.sendKeys("9899001068");
+		Password.clear();
+		Password.sendKeys("12345");
+		Submit.click();	
+		System.out.println(coformationmessage.getText());
+		return coformationmessage.getText();
+	}
+	public String RegFormPassword()
+	{
+		Registration.click();
+		Name.clear();
+		Name.sendKeys("Arun");
+		Email.clear();
+		Email.sendKeys("arun5603@gmail.com");
+		Mobile.clear();
+		Mobile.sendKeys("9899001068");
+	//	Password.clear();
+	//	Password.sendKeys("12345");
+		Submit.click();	
+		System.out.println(coformationmessage.getText());
+		return coformationmessage.getText();
+	}
+	public String RegForm()
+	{
+		Registration.click();
+		Name.clear();
+		Name.sendKeys("Arun");
+		Email.clear();
+		Email.sendKeys("arun5603@gmail.com");
+		Mobile.clear();
+		Mobile.sendKeys("9899001068");
+		Password.clear();
+		Password.sendKeys("12345");
+		Submit.click();	
+		System.out.println(coformationmessage.getText());
+		return coformationmessage.getText();
+	}
+	public void Login()
+	{
+		Login.click();
+		System.out.println("After Registration URL is :"+ driver.getCurrentUrl());
+		
+	}
 }
---
+++ src/test/java/com/asynctric/qa/Testcase/RegistrationPageTest.java
@@ -0,0 +1,84 @@
+package com.asynctric.qa.Testcase;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.asynctric.qa.base.TestBase;
+import com.asynctric.qa.pages.RegistrationPage;
+
+public class RegistrationPageTest extends TestBase{
+		
+		RegistrationPage regpage;
+	
+	public RegistrationPageTest() throws IOException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	@BeforeMethod
+	public void Setup() throws IOException
+	{
+		Initialization();
+		regpage= new RegistrationPage();
+	}
+	@Test(priority=1)
+	public void ValidateRegPageURLTest()
+	{
+		String currentURL= regpage.ValidateRegPageURL();
+		Assert.assertEquals(currentURL, "https://ecomm-fed59.web.app/register");
+		System.out.println("Current URL is :" + currentURL);
+	}
+	@Test(priority=2)
+	public void ValidateRegPageTextTest()
+	{
+		String text=regpage.ValidateRegPageText();
+		Assert.assertEquals(text, "Signup with Asyncentric");
+		System.out.println("Current Page text : " + text );
+	}
+	@Test(priority=3)
+	public void RegFormNameTest()
+	{
+		regpage.RegFormName();
+	}
+	@Test(priority=4)
+	public void RegFormEmailTest()
+	{
+		regpage.RegFormEmail();
+	}
+	@Test(priority=5)
+	public void RegFormMobileTest()
+	{
+		regpage.RegFormMobile();
+	}
+	@Test(priority=6)
+	public void RegFormPasswordTest()
+	{
+		regpage.RegFormPassword();
+	}  
+	@Test(priority=7)
+	public void RegFormTest()
+	{
+		
+		String conformationmsg=regpage.RegForm();
+		//regpage.RegForm();
+	//	Assert.assertEquals(conformationmsg, "Email already exists!");
+		if(conformationmsg.equals("Email already exists!")||conformationmsg.equals("Phone already exists!") )
+			regpage.Login();
+		
+		
+	}
+	
+	@AfterMethod
+	public void Teardown()
+	{
+		driver.quit();
+	}
+}
